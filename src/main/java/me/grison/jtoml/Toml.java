c52e9cbe5b914184e5d9515addd9f4ed2e3b6cdd
jbman <johbergmann@web.de>
2013-02-24 18:18:55 +0100

    Added .gitignore
    Added test for toml example file
    
    Removed apache commons dependencies by providing own specific unencoding and reading of file to string

--- src/main/java/me/grison/jtoml/Toml.java
+++ src/main/java/me/grison/jtoml/Toml.java
@@ -1,8 +1,7 @@
 package me.grison.jtoml;
 
-import org.apache.commons.io.FileUtils;
-
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.LinkedHashMap;
@@ -19,125 +18,156 @@ import java.util.regex.Pattern;
  *     toml.parse("pi = 3.14");
  *     toml.getFloat("pi");
  * </code>
- *
+ * 
  * @author $Author: alexandre grison$
-*/
+ */
 public class Toml implements Parser, Getter {
-    Map<String, Object> context = new LinkedHashMap<String, Object>();
-    final Matcher keyPathMatcher = Pattern.compile("((\\w+[.])+).*").matcher("");
+	Map<String, Object> context = new LinkedHashMap<String, Object>();
+	final Matcher keyPathMatcher = Pattern.compile("((\\w+[.])+).*")
+			.matcher("");
 
-    /**
-     * Creates a TOML instance loaded with the given String.
-     * @param string the TOML String to load.
-     * @return a TOM object instance
-     */
-    public static Toml parse(String string) {
-        return new Toml().parseString(string);
-    }
+	/**
+	 * Creates a TOML instance loaded with the given String.
+	 * 
+	 * @param string
+	 *            the TOML String to load.
+	 * @return a TOM object instance
+	 */
+	public static Toml parse(String string) {
+		return new Toml().parseString(string);
+	}
 
-    /**
-     * Creates a TOML instance loaded with the given file.
-     * @param file the TOML file to load
-     * @return a TOML object instance
-     * @throws IOException
-     */
-    public static Toml parse(File file) throws IOException {
-        return new Toml().parseFile(file);
-    }
+	/**
+	 * Creates a TOML instance loaded with the given file.
+	 * 
+	 * @param file
+	 *            the TOML file to load
+	 * @return a TOML object instance
+	 * @throws IOException
+	 */
+	public static Toml parse(File file) throws IOException {
+		return new Toml().parseFile(file);
+	}
 
-    @Override
-    public Toml parseString(String string) {
-        context = TomlParser.parse(string);
-        return this;
-    }
+	@Override
+	public Toml parseString(String string) {
+		context = TomlParser.parse(string);
+		return this;
+	}
 
-    @Override
-    public Toml parseFile(File file) throws IOException {
-        context = TomlParser.parse(FileUtils.readFileToString(file));
-        return this;
-    }
+	@Override
+	public Toml parseFile(File file) throws FileNotFoundException {
+		context = TomlParser.parse(Util.FileToString.read(file));
+		return this;
+	}
 
-    private String keyPath(String s) {
-        if (keyPathMatcher.reset(s).matches()) {
-            return keyPathMatcher.group(1).substring(0, keyPathMatcher.group(1).length() - 1);
-        } else {
-            return s;
-        }
-    }
+	private String keyPath(String s) {
+		if (keyPathMatcher.reset(s).matches()) {
+			return keyPathMatcher.group(1).substring(0,
+					keyPathMatcher.group(1).length() - 1);
+		} else {
+			return s;
+		}
+	}
 
-    @Override
-    public Object get(String key) {
-        if (key.contains(".")) {
-            String keyPath = keyPath(key);
-            return TomlParser.findContext(context, keyPath).get(key.replace(keyPath + ".", ""));
-        } else {
-            return context.get(key);
-        }
-    }
+	@Override
+	public Object get(String key) {
+		if (key.contains(".")) {
+			String keyPath = keyPath(key);
+			return TomlParser.findContext(context, keyPath).get(
+					key.replace(keyPath + ".", ""));
+		} else {
+			return context.get(key);
+		}
+	}
 
-    @Override
-    public String getString(String key) {
-        Object value = get(key);
-        if (value instanceof String) {
-            return (String) value;
-        } else {
-            throw new IllegalArgumentException("Value for key `" + key + "` is not of type String" + //
-                    (value == null ? "" : " but of type " + value.getClass().getName()));
-        }
-    }
+	@Override
+	public String getString(String key) {
+		Object value = get(key);
+		if (value instanceof String) {
+			return (String) value;
+		} else {
+			throw new IllegalArgumentException("Value for key `"
+					+ key
+					+ "` is not of type String"
+					+ //
+					(value == null ? "" : " but of type "
+							+ value.getClass().getName()));
+		}
+	}
 
-    @Override
-    public Integer getInt(String key) {
-        Object value = get(key);
-        if (value instanceof Integer) {
-            return (Integer) value;
-        } else {
-            throw new IllegalArgumentException("Value for key `" + key + "` is not of type Integer" + //
-                    (value == null ? "" : " but of type " + value.getClass().getName()));
-        }
-    }
+	@Override
+	public Integer getInt(String key) {
+		Object value = get(key);
+		if (value instanceof Integer) {
+			return (Integer) value;
+		} else {
+			throw new IllegalArgumentException("Value for key `"
+					+ key
+					+ "` is not of type Integer"
+					+ //
+					(value == null ? "" : " but of type "
+							+ value.getClass().getName()));
+		}
+	}
 
-    @Override
-    public Float getFloat(String key) {
-        Object value = get(key);
-        if (value instanceof Float) {
-            return (Float) value;
-        } else {
-            throw new IllegalArgumentException("Value for key `" + key + "` is not of type Float" + //
-                    (value == null ? "" : " but of type " + value.getClass().getName()));
-        }
-    }
+	@Override
+	public Float getFloat(String key) {
+		Object value = get(key);
+		if (value instanceof Float) {
+			return (Float) value;
+		} else {
+			throw new IllegalArgumentException("Value for key `"
+					+ key
+					+ "` is not of type Float"
+					+ //
+					(value == null ? "" : " but of type "
+							+ value.getClass().getName()));
+		}
+	}
 
-    @Override
-    public Calendar getDate(String key) {
-        Object value = get(key);
-        if (value instanceof Calendar) {
-            return (Calendar) value;
-        } else {
-            throw new IllegalArgumentException("Value for key `" + key + "` is not of type Calendar" + //
-                    (value == null ? "" : " but of type " + value.getClass().getName()));
-        }
-    }
+	@Override
+	public Calendar getDate(String key) {
+		Object value = get(key);
+		if (value instanceof Calendar) {
+			return (Calendar) value;
+		} else {
+			throw new IllegalArgumentException("Value for key `"
+					+ key
+					+ "` is not of type Calendar"
+					+ //
+					(value == null ? "" : " but of type "
+							+ value.getClass().getName()));
+		}
+	}
 
-    @Override
-    public List<Object> getList(String key) {
-        Object value = get(key);
-        if (value instanceof List) {
-            return (List) value;
-        } else {
-            throw new IllegalArgumentException("Value for key `" + key + "` is not of type List" + //
-                    (value == null ? "" : " but of type " + value.getClass().getName()));
-        }
-    }
+	@Override
+	public List<Object> getList(String key) {
+		Object value = get(key);
+		if (value instanceof List) {
+			return (List) value;
+		} else {
+			throw new IllegalArgumentException("Value for key `"
+					+ key
+					+ "` is not of type List"
+					+ //
+					(value == null ? "" : " but of type "
+							+ value.getClass().getName()));
+		}
+	}
 
-    @Override
-    public Boolean getBoolean(String key) {
-        Object value = get(key);
-        if (value instanceof Boolean) {
-            return (Boolean) value;
-        } else {
-            throw new IllegalArgumentException("Value for key `" + key + "` is not of type Boolean" + //
-                    (value == null ? "" : " but of type " + value.getClass().getName()));
-        }
-    }
+	@Override
+	public Boolean getBoolean(String key) {
+		Object value = get(key);
+		if (value instanceof Boolean) {
+			return (Boolean) value;
+		} else {
+			throw new IllegalArgumentException("Value for key `"
+					+ key
+					+ "` is not of type Boolean"
+					+ //
+					(value == null ? "" : " but of type "
+							+ value.getClass().getName()));
+		}
+	}
 }
